ab7c3a115f65ae841225faed0349fdb1bdef214a
Jack Kim <thejackkim@gmail.com>
2021-05-26 12:40:47 -0400

    Debrand for usability test

--- Snippet 2/Package 3.dart
+++ Snippet 2/Package 3.dart
@@ -1,12 +1,4 @@
-// Copyright 2021, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-// Auth example
-// Done using AutoRoute
-
 import 'package:auto_route/auto_route.dart';
-
 import 'package:flutter/material.dart';
 import 'sign_in_routing_auto_route.gr.dart';
 
